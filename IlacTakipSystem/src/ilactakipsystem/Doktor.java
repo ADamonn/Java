b585fe3616b094e43c04a0b7b52e14769a4849e5
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-05-15 18:38:21 +0300

    Start

---
+++ IlacTakipSystem/src/ilactakipsystem/Doktor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ilactakipsystem;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Doktor extends Kullanci {
+
+    String name, title;
+    int id, age;
+
+    public Doktor(String name, String title, int id, int age) {
+        super(name, title, id, age);
+    }
+}
