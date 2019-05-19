f3c2b0b9785e22de2350c99ca0b02331c7b45b6a
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-05-19 15:33:31 +0300

    edit

--- Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
+++ Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
@@ -15,37 +15,15 @@ public class Pharmacy implements Transactions{
     String location, name, date;
     int storage;
     Doctor Doktor;
-<<<<<<< HEAD
     ArrayList<Medicine> ilce=new ArrayList<>();
 
     @Override
     public void ilaceEkle(Medicine ilac) {
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
-    ArrayList<Medicine> ilce=new ArrayList<>();
-
-    @Override
-    public void ilaceEkle(Medicine ilac) {
-=======
-    ArrayList<Midicine> ilce=new ArrayList<>();
-
-    @Override
-    public void ilaceEkle(Midicine ilac) {
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-<<<<<<< HEAD
     public void ilaceSatin(Medicine ilec) {
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
-    public void ilaceSatin(Medicine ilec) {
-=======
-    public void ilaceSatin(Midicine ilec) {
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -55,15 +33,7 @@ public class Pharmacy implements Transactions{
     }
 
     @Override
-<<<<<<< HEAD
     public void ilacArama(Medicine ilac) {
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
-    public void ilacArama(Medicine ilac) {
-=======
-    public void ilacArama(Midicine ilac) {
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -73,29 +43,13 @@ public class Pharmacy implements Transactions{
     }
 
     @Override
-<<<<<<< HEAD
     public void ilacSillme(Medicine ilac) {
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
-    public void ilacSillme(Medicine ilac) {
-=======
-    public void ilacSillme(Midicine ilac) {
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-<<<<<<< HEAD
     public int ilacesayisi(Medicine ilac) {
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
-    public int ilacesayisi(Medicine ilac) {
-=======
-    public int ilacesayisi(Midicine ilac) {
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Pharmacy.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
--- Pharmaceutical tracking/src/ilactakipsystem/Transactions.java
+++ Pharmaceutical tracking/src/ilactakipsystem/Transactions.java
@@ -12,11 +12,6 @@ import java.util.ArrayList;
  * @author ajohanson
  */
 public interface Transactions {
-<<<<<<< HEAD
-    public void ilaceEkle(Medicine ilac);
-    public void ilaceSatin(Medicine ilec);
-=======
-<<<<<<< HEAD:Pharmaceutical tracking/src/ilactakipsystem/Transactions.java
     public void ilaceEkle(Medicine ilac);
     public void ilaceSatin(Medicine ilec);
     public void ilacSiralama(ArrayList arrayList);
@@ -24,20 +19,5 @@ public interface Transactions {
     public void ilacArama (String SeriNumber);
     public void ilacSillme (Medicine ilac);
     public int ilacesayisi (Medicine ilac);
-=======
-    public void ilaceEkle(Midicine ilac);
-    public void ilaceSatin(Midicine ilec);
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
-    public void ilacSiralama(ArrayList arrayList);
-    public void ilacArama (Medicine ilac);
-    public void ilacArama (String SeriNumber);
-<<<<<<< HEAD
-    public void ilacSillme (Medicine ilac);
-    public int ilacesayisi (Medicine ilac);
-=======
-    public void ilacSillme (Midicine ilac);
-    public int ilacesayisi (Midicine ilac);
->>>>>>> 81f9502cd3c4cf18789609d06185f26bf0003c21:Pharmaceutical tracking/src/ilactakipsystem/Transactions.java
->>>>>>> 46e2b04469b551ea77d91bbcf4ace061f0ae57d2
-    
+   
 }
